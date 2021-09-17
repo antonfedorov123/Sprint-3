4ce285db9f3074db505f0e203ac1bc0dc1895b57
Anton Fedorov <avyacfedorov@sberbank.ru>
2021-09-17 19:50:10 +0300

    +io

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,17 +1,38 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
-class Archivator {
+object Archivator {
+
+    const val FILE_NAME: String = "logfile.log"
+    const val ZIP_NAME: String = "logfile.zip"
+    const val UNZIPPED_FILE_NAME: String = "unzippedLogfile.log"
 
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        ZipOutputStream(FileOutputStream(ZIP_NAME)).use { zipOutputStream ->
+            FileInputStream(FILE_NAME).use { fileInputStream ->
 
+                val entry = ZipEntry(UNZIPPED_FILE_NAME)
+                zipOutputStream.putNextEntry(entry)
+
+                val buffer = ByteArray(fileInputStream.available())
+                fileInputStream.read(buffer)
+                zipOutputStream.write(buffer)
+            }
+        }
     }
 
     /**
@@ -20,5 +41,29 @@ class Archivator {
      */
     fun unzipLogfile() {
 
+        if(File(ZIP_NAME).exists()) {
+            println("Nooo")
+        }
+
+        ZipInputStream(FileInputStream(ZIP_NAME))
+            .use { zipInputStream ->
+                FileOutputStream(UNZIPPED_FILE_NAME).use { fileOutputStream ->
+
+                    while (zipInputStream.nextEntry != null) {
+                        var char = zipInputStream.read()
+                        while (char != -1) {
+                            fileOutputStream.write(char)
+                            char = zipInputStream.read()
+                        }
+                    }
+
+                }
+            }
     }
+
+}
+
+fun main() {
+    Archivator.zipLogfile()
+    Archivator.unzipLogfile()
 }
