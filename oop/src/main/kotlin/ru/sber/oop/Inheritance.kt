38c8b8000c3f9c70b70e99292d5ffa66063827e9
Anton Fedorov <avyacfedorov@sberbank.ru>
2021-09-17 17:29:56 +0300

    +oop

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,19 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int, val monster: Monster = Goblin("Gazlowe", "The Creator of Orgrimmar", "EPIC", 99999)) {
 
-    val dangerLevel = 5
+    constructor(name: String) : this(name, 100)
+
+    protected open val dangerLevel = 5
 
     fun description() = "Room: $name"
 
-    fun load() = "Nothing much to see here..."
+    open fun load() = "Nothing much to see here..."
 
 }
 
-//TODO: create class TownSquare here...
+class TownSquare : Room("Town Square", 1000) {
+    override val dangerLevel = super.dangerLevel - 3
+
+    override fun load() = "Hop! Hey! La-la-ley!"
+}
